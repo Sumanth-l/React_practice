fecffe84799384c792ac4f4612d8d6da2d45933b
sumanth <sumanthsumanthacharya8@gmail.com>
2026-02-09 13:00:56 +0530

    implemented useRef hook

--- src/components/UseState.tsx
+++ src/components/UseState.tsx
@@ -1,4 +1,4 @@
-import {useContext,useMemo , useEffect, useState, createContext } from "react"
+import {useContext,useMemo , useEffect, useState, createContext, useRef } from "react"
 import { intialItems } from "./Utils";
 import Component1 from "./Component1";
 
@@ -10,11 +10,22 @@ export const userContext=createContext<string>("")
 const user="sumanth"
 
 export default function UseState(){
+
+    const countRef=useRef(0);
+
     const[count,setCount]=useState(0);
     const[items]=useState(intialItems)
 
 
     const selectedItems=useMemo(()=>items.find((item)=>item.isSelected),[items])
+
+
+
+     const handleIncrement=()=>{
+        setCount(count+1);
+        countRef.current++;
+        console.log(countRef)
+     }
 
     useEffect(()=>{
     console.log("component rerendered");
@@ -29,7 +40,7 @@ export default function UseState(){
     return(
         <div>
             <p>count : {count}</p>
-            <button onClick={()=>setCount((count)=>count+1)}>Increment</button>
+            <button onClick={handleIncrement}>Increment</button>
             <button onClick={()=>setCount((count)=>count-1)}>Decrement</button>
             <button onClick={()=>setCount(0)}>Reset</button>
             <h1>selectedItems:{selectedItems?.id}</h1>
