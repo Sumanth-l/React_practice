cb5a6b2b27cbd82333d247ac4d668002d0100d9a
sumanth <sumanthsumanthacharya8@gmail.com>
2026-02-12 10:51:41 +0530

    implemented useReducer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
+import { StrictMode, use } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {configureStore} from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import userReducer from './features/user.tsx'
+
+
+const store=configureStore({
+  reducer:{
+    user:userReducer,
+  }
+})
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
